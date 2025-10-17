dd1f9571550bee3dde919ea66f7a70e3b7f99893
TryYourBestAndLeaveTheRest <sherifdeenraji96@gmail.com>
2025-10-17 15:43:41 +0100

    feat: add express-rate-limit middleware to limit request rate and enhance server security

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { fetchWithTimeout } from './helper/apiCall.js';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 // Basic logging
 app.use(morgan(process.env.LOG_FORMAT || 'dev'));
@@ -36,7 +47,7 @@ app.get('/me', async (req, res) => {
   } catch (err) {
     console.error('Failed to fetch cat fact:', err.message || err);
     // Fallback message
-    fact ='Could not fetch a cat fact at this time.';
+    fact = 'Could not fetch a cat fact at this time.';
   }
 
   const payload = {
